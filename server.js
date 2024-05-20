e1109c5f844690c2268e327eb6f1230c281d0a9e
oscarm1990 <124219163+oscarm1990@users.noreply.github.com>
2024-05-20 15:57:16 +1000

    Update server.js

--- server.js
+++ server.js
@@ -10,11 +10,15 @@ app.use(express.json());
 app.get('/users', function (req, res) {
     fs.readFile(__dirname + "/users.json", 'utf8', function (err, data) {
         if (err) {
-            res.status(500).send('Error reading file');
+            res.status(500).json({error: 'Error reading file'});
             return;
         }
-        console.log(data);
-        res.send(data); // it's okay to use res.send() directly
+        try {
+            const users = JSON.parse(data);
+            res.json(users);
+        } catch (e) {
+            res.status(500).json({error: 'Error parsing JSON data'});
+        }
     });
 });
 
@@ -22,28 +26,24 @@ app.get('/users', function (req, res) {
 app.get('/users/:id', function (req, res) {
     fs.readFile(__dirname + "/users.json", 'utf8', function (err, data) {
         if (err) {
-            res.status(500).send('Error reading file');
+            res.status(500).json({error: 'Error reading file'});
             return;
         }
         
-        let users;
         try {
-            users = JSON.parse(data);
+            const users = JSON.parse(data);
+            const userKey = "user" + req.params.id;
+            const user = users[userKey];
+            
+            if (!user) {
+                res.status(404).json({error: 'User not found'});
+                return;
+            }
+            
+            res.json(user); // using res.json() will correctly set Content-Type header
         } catch (e) {
-            res.status(500).send('Error parsing JSON data');
-            return;
+            res.status(500).json({error: 'Error parsing JSON data'});
         }
-
-        const userKey = "user" + req.params.id;
-        const user = users[userKey];
-        
-        if (!user) {
-            res.status(404).send('User not found');
-            return;
-        }
-        
-        console.log(user);
-        res.json(user); // using res.json() will correctly set Content-Type header
     });
 });
 
@@ -52,37 +52,27 @@ app.post('/users', function (req, res) {
     // Read existing users
     fs.readFile(__dirname + "/users.json", 'utf8', function (err, data) {
         if (err) {
-            res.status(500).send('Error reading file');
+            res.status(500).json({error: 'Error reading file'});
             return;
         }
 
-        let users;
         try {
-            users = JSON.parse(data);
+            const users = JSON.parse(data);
+            const newUser = req.body;
+            const newUserId = "user" + (Object.keys(users).length + 1);
+            newUser.id = Object.keys(users).length + 1; // Set the user ID
+
+            users[newUserId] = newUser;
+            fs.writeFile(__dirname + "/users.json", JSON.stringify(users, null, 2), 'utf8', function (err) {
+                if (err) {
+                    res.status(500).json({error: 'Error writing to file'});
+                    return;
+                }
+                res.status(201).json(newUser); // Return the new user data with a 201 Created status code
+            });
         } catch (e) {
-            res.status(500).send('Error parsing JSON data');
-            return;
+            res.status(500).json({error: 'Error parsing JSON data'});
         }
-
-        // Extract the new user data from the request body
-        const newUser = req.body;
-
-        // Assign a new unique user ID
-        const newUserId = "user" + (Object.keys(users).length + 1);
-        newUser.id = Object.keys(users).length + 1; // Set the user ID
-
-        // Add the new user to the existing user list
-        users[newUserId] = newUser;
-
-        // Write the updated users to the file
-        fs.writeFile(__dirname + "/users.json", JSON.stringify(users, null, 2), 'utf8', function (err) {
-            if (err) {
-                res.status(500).send('Error writing to file');
-                return;
-            }
-
-            res.status(201).json(newUser); // Return the new user data with a 201 Created status code
-        });
     });
 });
 
